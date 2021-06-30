1d13b4f15bb3d90a3afc47da79b82f296e4243e7
杨帅 <yangshuai@xiaoniangao.com>
2021-06-30 21:53:55 +0800

    [M] 源码解读

--- composer.js
+++ composer.js
@@ -5,11 +5,14 @@ var minify = require('./lib/minify');
 module.exports = function(uglify, logger) {
   return function(opts) {
     var minifier = minify(uglify, logger)(opts);
+    // through2.obj(transform, flush) 返回一个转换流 stream._transform = transform
+    // transform(chunk, encoding, callback) // transform 的 3 个参数分别是 chunk, encoding, callback
     return through.obj(function(file, encoding, callback) {
+      // file 是 Vinyl
       var newFile = null;
       var err = null;
       try {
-        newFile = minifier(file);
+        newFile = minifier(file); // transform 方法中通过 uglyfy-js 来压缩代码
       } catch (e) {
         err = e;
       }
